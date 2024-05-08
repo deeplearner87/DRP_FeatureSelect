49f0a086740c1672dbf8b58b938ed6e4cea42592
Dibyendu Seal <dibs.adi@gmail.com>
2024-05-08 13:03:44 +0200

    Add files via upload

--- DRP_analysis_streamlit_v2.py
+++ DRP_analysis_streamlit_v2.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import plotly.io as pio
-pio.renderers.default = 'iframe'
-import plotly.express as px
+#import plotly.io as pio
+#pio.renderers.default = 'iframe'
+#import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
